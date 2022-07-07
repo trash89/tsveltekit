4f63ebbfa3dc8fd8650f1909bdc40f0be18d13e0
m r <trash89@laposte.net>
2022-07-07 08:18:03 +0200

    endpoints

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,3 @@
-<script context="module">
-  export const load = async ({ fetch }) => {
-    const res = await fetch("/posts");
-    if (res.ok) {
-      const { token, profile } = await res.json();
-      return { props: { token, profile } };
-    }
-  };
-</script>
-
 <script>
   export let token;
   export let profile;
@@ -18,3 +8,4 @@
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 <pre>{JSON.stringify(profile)}</pre>
+<pre>{JSON.stringify(token)}</pre>
